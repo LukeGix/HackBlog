a7f6375be55f8a5889fa2e2c59675342ca31e7b2
LukeGix <80392368+LukeGix@users.noreply.github.com>
2021-05-29 10:31:55 +0200

    Add files via upload

--- database.ts
+++ database.ts
@@ -2,15 +2,15 @@ import * as User from './models/user';
 import * as Cookie from './models/cookie';
 import * as Blog from './models/blog';
 let mongoose = require('mongoose');
+let crypto = require('crypto');
 
 let dburi : string = "mongodb://localhost:27017/LocalApp";
 
-let GetPass : Function = function f(callback : Function){
+let GetPass : Function = function f(callback : Function, params){
 		mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true});
 		let conn = mongoose.connection;
 		conn.once('open', () => {
-			console.log('[*]Connesso al db[*]');
-			User.findOne({name:'admin'} , (err, data) =>{
+			User.findOne({name: params.name, password: crypto.createHash('md5').update(params.password).digest('hex')} , (err, data) =>{
 				if(err) throw err;
 				callback(data);
 				conn.close();
@@ -23,7 +23,6 @@ let GetCookie : Function = function(callback : Function){
 		mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true});
 		let conn = mongoose.connection;
 		conn.once('open', () => {
-			console.log('[*]Connesso al db[*]');
 			Cookie.findOne({role:'admin'} , (err, data) =>{
 				if(err) throw err;
 				callback(data.password);
@@ -32,13 +31,12 @@ let GetCookie : Function = function(callback : Function){
 		})
 
 		
-	}
-//Questa è per trovare un solo blog element, devo inserire anche quella per trovarli tutti
+}
+
 let GetBlog : Function = function(callback : Function, params){
 		mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true});
 		let conn = mongoose.connection;
 		conn.once('open', () => {
-			console.log('[*]Connesso al db[*]');
 			if(params !== null){
 				Blog.findOne(params , (err, data) =>{
 					callback(data);
@@ -57,11 +55,38 @@ let GetBlog : Function = function(callback : Function, params){
 			}
 		})
 
-	}
+}
 
+let SetBlog : Function = function(params : object){
+	mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true});
+	let conn = mongoose.connection;
+	conn.once('open', () => {
+		let us = new Blog(params);
+		us.save((err, doc, num) => {
+			if(err) throw err;
+			console.log('salvataggio effettuato con successo');
+			conn.close();
+		});
+	})
+}
+
+let SetUser : Function = function(params : object){
+	mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true});
+	let conn = mongoose.connection;
+	conn.once('open', () => {
+		let us = new User(params);
+		us.save((err, doc, num) => {
+			if(err) throw err;
+			console.log('salvataggio effettuato con successo');
+			conn.close();
+		});
+	})
+}
 
 export {
 	GetPass as GPass,
 	GetCookie as GCookie,
-	GetBlog as GBlog
+	GetBlog as GBlog,
+	SetBlog as SBlog,
+	SetUser as SUser
 }
